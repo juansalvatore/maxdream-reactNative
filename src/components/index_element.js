304dc235b80f7eb55e73a1ee44e463f256a3df33
Juan Salvatore <juansalvatore@live.com.ar>
2018-02-03 23:44:51 -0300

    Create index elements

--- src/components/index_element.js
+++ src/components/index_element.js
@@ -2,58 +2,128 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { TouchableOpacity, Text, View } from 'react-native'
 
+import Row from './Row'
+
 import Icon from 'react-native-vector-icons/FontAwesome'
 
 // este comando solucion error 500 en caso de aparecer
 // rm ./node_modules/react-native/local-cli/core/__fixtures__/files/package.json
 
+// https://oblador.github.io/react-native-vector-icons/
+
 IndexElement = props => {
   const { containerStyle, leftContainer, rightContainer } = styles
+  const size = 28
   return (
-    <View>
-      <View style={containerStyle}>
-        <View style={leftContainer}>
-          <Icon name="glass" size={20} color="black" />
+    <Row>
+      <View style={{ flex: 0 }}>
+        <View style={containerStyle}>
+          <View style={leftContainer}>
+            <Icon name="bus" size={size} color="black" />
+            <Icon name="plane" size={size} color="black" />
+          </View>
+          <View style={{ marginLeft: 20, width: 1, backgroundColor: '#ccc' }} />
+          <View style={rightContainer}>
+            <Text>Icon</Text>
+          </View>
         </View>
-        <View style={rightContainer}>
-          <Text>Icon</Text>
+
+        <View style={containerStyle}>
+          <View style={leftContainer}>
+            <Icon name="hotel" size={size} color="black" />
+          </View>
+          <View style={{ marginLeft: 20, width: 1, backgroundColor: '#ccc' }} />
+          <View style={rightContainer}>
+            <Text>Icon</Text>
+            <Text>Icon</Text>
+            <Text>Icon</Text>
+            <Text>Icon</Text>
+          </View>
+        </View>
+
+        <View style={containerStyle}>
+          <View style={leftContainer}>
+            <Icon name="globe" size={size} color="black" />
+          </View>
+          <View style={{ marginLeft: 20, width: 1, backgroundColor: '#ccc' }} />
+          <View style={rightContainer}>
+            <Text>Icon</Text>
+            <Text>Icon</Text>
+            <Text>Icon</Text>
+            <Text>Icon</Text>
+          </View>
         </View>
       </View>
-      <View style={containerStyle}>
-        <View style={leftContainer}>
-          <Text>Icon</Text>
+      <View>
+        <View style={containerStyle}>
+          <View style={leftContainer}>
+            <Icon name="male" size={size} color="black" />
+          </View>
+          <View style={{ marginLeft: 20, width: 1, backgroundColor: '#ccc' }} />
+          <View style={rightContainer}>
+            <Text>Icon</Text>
+            <Text>Icon</Text>
+            <Text>Icon</Text>
+            <Text>Icon</Text>
+          </View>
         </View>
-        <View style={rightContainer}>
-          <Text>Icon</Text>
-          <Text>Icon</Text>
-          <Text>Icon</Text>
-          <Text>Icon</Text>
+
+        <View style={containerStyle}>
+          <View style={leftContainer}>
+            <Icon name="tree" size={size} color="black" />
+          </View>
+          <View style={{ marginLeft: 20, width: 1, backgroundColor: '#ccc' }} />
+          <View style={rightContainer}>
+            <Text>Icon</Text>
+            <Text>Icon</Text>
+            <Text>Icon</Text>
+            <Text>Icon</Text>
+          </View>
+        </View>
+
+        <View style={containerStyle}>
+          <View style={leftContainer}>
+            <Icon name="leaf" size={size} color="black" />
+            <Icon name="safari" size={size} color="black" />
+            <Icon name="paper-plane-o" size={size} color="black" />
+          </View>
+          <View style={{ marginLeft: 20, width: 1, backgroundColor: '#ccc' }} />
+          <View style={rightContainer}>
+            <Text>Icon</Text>
+            <Text>Icon</Text>
+            <Text>Icon</Text>
+            <Text>Icon</Text>
+          </View>
         </View>
       </View>
-    </View>
+    </Row>
   )
 }
 
 const styles = {
   containerStyle: {
-    width: 300,
+    width: 370,
     minHeight: 100,
     flexDirection: 'row',
     backgroundColor: '#ccc',
     justifyContent: 'center',
     marginBottom: 5,
-    padding: 10,
+    padding: 0,
     borderWidth: 1,
     borderRadius: 2,
     borderColor: '#ddd',
     backgroundColor: 'white',
+    marginRight: 5,
   },
   leftContainer: {
-    flex: 1,
-    justifyContent: 'center',
+    flex: 0.9,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
     alignItems: 'center',
-    backgroundColor: 'pink',
-    borderRadius: 50,
+    backgroundColor: 'white',
+    borderRadius: 0,
+    marginLeft: 25,
+    height: 180,
   },
   rightContainer: {
     flex: 2,
